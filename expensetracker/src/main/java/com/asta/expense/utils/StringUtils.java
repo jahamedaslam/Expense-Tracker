061d071656944017b3d76ae591a4bf2603970b28
Aslam <jahamedaslam@gmail.com>
2024-07-18 23:28:17 +0530

    First Review

--- expensetracker/src/main/java/com/asta/expense/utils/StringUtils.java
+++ expensetracker/src/main/java/com/asta/expense/utils/StringUtils.java
@@ -3,6 +3,6 @@ package com.asta.expense.utils;
 import java.time.LocalDateTime;
 
 public class StringUtils {
-    public static String user = "ASTA";
+    public static String user = "vidh";
     public static LocalDateTime now = LocalDateTime.now();
 }
